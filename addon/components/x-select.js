82376d0029a955cf42af1a6aa726e8ca49e992f0
Eric Polino <epolino@vikus.com>
2017-01-06 14:49:44 -0500

    Don't need to check oldValue in didUpdateAttrs

--- addon/components/x-select.js
+++ addon/components/x-select.js
@@ -101,10 +101,9 @@ export default Component.extend({
       this.open();
     }
 
-    // Update input if value has changed
+    // Update input if label has changed
     let newValue = attrs.newAttrs.value;
-    let oldValue = attrs.oldAttrs.value;
-    if (oldValue && newValue) {
+    if (newValue) {
       let { label } = this.retrieveOption(newValue.value);
       if (label !== this.get('token')) {
         this.setOption(newValue.value);
