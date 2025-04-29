2ddbaa2829f83a04c96fcebf2d2b380f5257c8a6
btecu <github@bujorel.net>
2025-04-30 02:22:06 +0300

    Make component template only

--- addon/components/select-dropdown-option.js
+++
@@ -1,6 +0,0 @@
-import Component from '@ember/component';
-
-export default class SelectDropdownOptionComponent extends Component {
-  classNames = ['es-option'];
-  classNameBindings = ['model.isSelected:es-highlight'];
-}
