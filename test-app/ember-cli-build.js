ea9524c195772342a2fcab093dac4e01596ffee7
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-11-09 11:26:58 -0500

    Fix eslint

--- test-app/ember-cli-build.js
+++ test-app/ember-cli-build.js
@@ -1,16 +1,11 @@
-'use strict';;
+'use strict';
 const sideWatch = require('@embroider/broccoli-side-watch');
-const { maybeEmbroider } = require('@embroider/test-setup');
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-const {
-  compatBuild
-} = require("@embroider/compat");
+const { compatBuild } = require('@embroider/compat');
 
-module.exports = async function(defaults) {
-  const {
-    buildOnce
-  } = await import("@embroider/vite");
+module.exports = async function (defaults) {
+  const { buildOnce } = await import('@embroider/vite');
 
   const app = new EmberApp(defaults, {
     // Add options here
