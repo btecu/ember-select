9cac87b5e0f3a26bc021c06050a45e0984c2c4c7
btecu <github@bujorel.net>
2025-11-26 01:06:31 +0200

    Lint Update dependencies

--- test-app/app/config/environment.js
+++ test-app/app/config/environment.js
@@ -3,25 +3,18 @@ import { assert } from '@ember/debug';
 
 const config = loadConfigFromMeta('test-app');
 
-assert(
-  'config is not an object',
-  typeof config === 'object' && config !== null
-);
+assert('config is not an object', typeof config === 'object' && config !== null);
 assert(
   'modulePrefix was not detected on your config',
-  'modulePrefix' in config && typeof config.modulePrefix === 'string'
+  'modulePrefix' in config && typeof config.modulePrefix === 'string',
 );
+
 assert(
   'locationType was not detected on your config',
-  'locationType' in config && typeof config.locationType === 'string'
+  'locationType' in config && typeof config.locationType === 'string',
 );
-assert(
-  'rootURL was not detected on your config',
-  'rootURL' in config && typeof config.rootURL === 'string'
-);
-assert(
-  'APP was not detected on your config',
-  'APP' in config && typeof config.APP === 'object'
-);
+
+assert('rootURL was not detected on your config', 'rootURL' in config && typeof config.rootURL === 'string');
+assert('APP was not detected on your config', 'APP' in config && typeof config.APP === 'object');
 
 export default config;
